725ed0f902fb7d86f6f274e00bfefb69f72146b1
IliyaKr <99329456+IliyaKr@users.noreply.github.com>
2022-03-23 13:53:43 +0300

    1
    begining code

---
+++ calculatoreasy.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h> /* Стандартный заголовочный файл ввода-вывода */
+#include <iostream> /* Библиотека (стандарт) */
+#include <locale.h> /* Русификатор */
+#include <windows.h> /* Русификатор */
+using namespace std;
